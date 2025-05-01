e84a4af5ea7c79640109d2285dd287579ae72868
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-05-01 10:45:04 -0500

    Creacion de clase MedicalConsultation

---
+++ sav-mvsc-medicalcontrol/src/main/java/com/jr/sav_mvsc_medicalcontrol/models/MedicalConsultation.java
@@ -0,0 +1,40 @@
+package com.jr.sav_mvsc_medicalcontrol.models;
+
+import java.time.LocalDate;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "medical_consultations")
+public class MedicalConsultation {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "mcon_id_consultatio")
+    private Long idConsultation;
+
+    @Column(name = "mcon_id_pet")
+    private Long idPet;
+
+    @Column(name = "mcon_date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "mcon_reason", length = 20, nullable = false)
+    private String reason;
+
+    @Column(name = "mcon_observatio", length = 30, nullable = false)
+    private String observations;
+
+    @Column(name = "mecon_veterinary_id", nullable = false)
+    private Long veterinaryId;
+}
