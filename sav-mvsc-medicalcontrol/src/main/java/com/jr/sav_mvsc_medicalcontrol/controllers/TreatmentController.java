06e37924c5d6388629d46f6b3e8e8d178b8172d7
Siven06 <geyllermojica2006@gmail.com>
2025-05-03 12:51:13 -0500

    creacion de la calse ontroladora Treatment

---
+++ sav-mvsc-medicalcontrol/src/main/java/com/jr/sav_mvsc_medicalcontrol/controllers/TreatmentController.java
@@ -0,0 +1,9 @@
+package com.jr.sav_mvsc_medicalcontrol.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/sav/treatment")
+public class TreatmentController {
+}
