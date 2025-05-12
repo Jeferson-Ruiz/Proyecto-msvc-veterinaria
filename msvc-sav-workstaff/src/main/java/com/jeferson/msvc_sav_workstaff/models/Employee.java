dc6a17af8f25627c6acea16b90f0c9c3c5362ad3
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-05-12 15:25:34 -0500

    Creacion de la clase abstract Employee

---
+++ msvc-sav-workstaff/src/main/java/com/jeferson/msvc_sav_workstaff/models/Employee.java
@@ -0,0 +1,54 @@
+package com.jeferson.msvc_sav_workstaff.models;
+
+import java.time.LocalDate;
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "emp_job_position",discriminatorType = DiscriminatorType.STRING)
+@Table(name = "employees")
+public abstract class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "emp_id")
+    private Long id;
+
+    @Column(name = "emp_name", length = 20, nullable = false)
+    private String name;
+
+    @Column(name = "emp_last_name", length = 20, nullable = false)
+    private String lastName;
+
+    @Column(name = "emp_docu_type", nullable = false, length = 20)
+    private String documentType;
+
+    @Column(name = "emp_docu_number", nullable = false, unique = true)
+    private Long documentNumber;
+
+    @Column(name = "emp_date_of_birth", nullable = false)
+    private LocalDate dateOfBirth;
+
+    @Column(name = "emp_email", nullable = false, length = 50)
+    private String email;
+
+    @Column(name = "emp_phone_number", nullable = false)
+    private Long phoneNumber;
+
+    @Column(name = "emp_contract_type", nullable = false, length = 20)
+    private String contractType;
+
+}
