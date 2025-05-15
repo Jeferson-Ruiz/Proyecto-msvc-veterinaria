c7e9f770697ccdbbb3fe08bf63e939b01d6e2613
Luisfer05 <luisgodoirojas@gmail.com>
2025-05-14 20:59:47 -0500

    agregar metodos personalizado al repository

--- msvc-sav-workstaff/src/main/java/com/jeferson/msvc_sav_workstaff/repositories/EmployeeRespository.java
+++ msvc-sav-workstaff/src/main/java/com/jeferson/msvc_sav_workstaff/repositories/EmployeeRespository.java
@@ -1,8 +1,24 @@
 package com.jeferson.msvc_sav_workstaff.repositories;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.jeferson.msvc_sav_workstaff.models.Employee;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 public interface EmployeeRespository extends CrudRepository<Employee, Long> {
+    List<Employee> findByJobPosition(String jobPosition);
 
+    @Modifying
+    @Query("update Employee set email=:email where idEmployee=:idEmployee")
+    void updateEmail(@Param("idEmployee") Long idEmployee, @Param("email") String email);
+
+    @Modifying
+    @Query("update Employee set phoneNumber=:phoneNumber where idEmployee=:idEmployee" )
+    void updatePhoneNumber(@Param("idEmployee")Long idEmployee, @Param("phoneNumber") Long phoneNumber);
+
+    @Modifying
+    @Query("update Employee set contractType=:contractType where idEmployee=:idEmployee")
+    void updateContractType (@Param("idEmployee") Long idEmployee, @Param("contractType") String contractType);
 }
