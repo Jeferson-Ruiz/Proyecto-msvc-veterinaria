1e9c4e00075498f2cfe7b150db12d84807530ae1
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-05-16 15:33:21 -0500

    Creacion de clase EmployeeController

---
+++ msvc-sav-workstaff/src/main/java/com/jeferson/msvc_sav_workstaff/controllers/EmployeeController.java
@@ -0,0 +1,99 @@
+package com.jeferson.msvc_sav_workstaff.controllers;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.jeferson.msvc_sav_workstaff.models.ContractType;
+import com.jeferson.msvc_sav_workstaff.models.Employee;
+import com.jeferson.msvc_sav_workstaff.services.EmployeeService;
+
+@RestController
+@RequestMapping("api/sav/employee")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployee() {
+        return ResponseEntity.ok(employeeService.findAllEmployee());
+    }
+
+    @GetMapping("id/{idEmployee}")
+    public ResponseEntity<?> getEmployeeById(@PathVariable Long idEmployee) {
+        Optional<Employee> optEmployee = employeeService.findEmployeeById(idEmployee);
+
+        if (optEmployee.isPresent()) {
+            return ResponseEntity.ok(optEmployee.get());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("El empleado con el Id: " + idEmployee + " no se encuenta registrado en el sistema");
+    }
+
+    @GetMapping("jobposition/{jobPosition}")
+    public ResponseEntity<List<Employee>> getAllEmployeeByJonPosition(@PathVariable String jobPosition) {
+        return ResponseEntity.ok(employeeService.FindAllByJobPosition(jobPosition));
+    }
+
+    @PostMapping
+    public ResponseEntity<Employee> saveInfoEmployee(@RequestBody Employee employee) {
+        return new ResponseEntity<>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("id/{idEmployee}")
+    public ResponseEntity<?> deleteInfoEmployee(@PathVariable Long idEmployee) {
+        Optional<Employee> optEmployee = employeeService.findEmployeeById(idEmployee);
+
+        if (optEmployee.isPresent()) {
+            employeeService.deleteEmployee(idEmployee);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Eliminación no valida. El empleado con el Id: " + idEmployee + " no existe en el sistema");
+    }
+
+    @PatchMapping("update/email/{idEmployee}")
+    public ResponseEntity<?> updateInfoEmailEmployee(@PathVariable Long idEmployee, @RequestBody String email) {
+        if (employeeService.findEmployeeById(idEmployee).isPresent()) {
+            employeeService.updateEmail(idEmployee, email);
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Actualización no valida. El empleado con el Id: " + idEmployee + " no existe en el sistema");
+    }
+
+    @PatchMapping("update/phonenumber/{idEmployee}")
+    public ResponseEntity<?> updateInfoNumberPhoneEmployee(@PathVariable Long idEmployee,
+            @RequestBody Long numberPhone) {
+        if (employeeService.findEmployeeById(idEmployee).isPresent()) {
+            employeeService.updateNumberPhone(idEmployee, numberPhone);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Actualización no valida. El empleado con el Id: " + idEmployee + " no existe en el sistema");
+    }
+
+    @PatchMapping("ipdate/contractype/{idEmployee}")
+    public ResponseEntity<?> updateInfoContractType(@PathVariable Long idEmployee,
+            @RequestBody ContractType contractType) {
+        if (employeeService.findEmployeeById(idEmployee).isPresent()) {
+            employeeService.updateContractType(idEmployee, contractType);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Actualización no valida. El empleado con el Id: " + idEmployee + " no existe en el sistema");
+    }
+}
