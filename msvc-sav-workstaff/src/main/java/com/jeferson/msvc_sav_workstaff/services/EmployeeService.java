2bb6998cc1a8bb3fd87dfbe3d58279af8ff0cd0e
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-05-16 12:30:05 -0500

    Terminacion de metodos de la clase service

--- msvc-sav-workstaff/src/main/java/com/jeferson/msvc_sav_workstaff/services/EmployeeService.java
+++ msvc-sav-workstaff/src/main/java/com/jeferson/msvc_sav_workstaff/services/EmployeeService.java
@@ -1,4 +1,25 @@
 package com.jeferson.msvc_sav_workstaff.services;
 
+import java.util.List;
+import java.util.Optional;
+import com.jeferson.msvc_sav_workstaff.models.Employee;
+
 public interface EmployeeService {
+
+    List<Employee> findAllEmployee();
+
+    Optional<Employee> findEmployeeById(Long idEmployee);
+
+    List<Employee> FindAllByJobPosition(String jobPosition);
+
+    Employee saveEmployee(Employee employee);
+
+    void deleteEmployee(Long idEmployee);
+
+    void updateEmail(Long idEmployee, String email);
+
+    void updateNumberPhone(Long idEmployee, Long phoneNumber);
+
+    void updateContractType(Long idEmployee, String contractType);
+
 }
--- msvc-sav-workstaff/src/main/java/com/jeferson/msvc_sav_workstaff/services/EmployeeServiceImpl.java
+++ msvc-sav-workstaff/src/main/java/com/jeferson/msvc_sav_workstaff/services/EmployeeServiceImpl.java
@@ -3,7 +3,7 @@ package com.jeferson.msvc_sav_workstaff.services;
 import com.jeferson.msvc_sav_workstaff.models.Employee;
 import com.jeferson.msvc_sav_workstaff.repositories.EmployeeRespository;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,26 +16,46 @@ public class EmployeeServiceImpl implements EmployeeService {
         this.employeeRespository = employeeRespository;
     }
 
+    @Override
     public List<Employee> findAllEmployee(){
         return (List<Employee>)  employeeRespository.findAll();
     }
 
+    @Override
     public Optional<Employee> findEmployeeById(Long idEmployee){
         return  employeeRespository.findById(idEmployee);
     }
 
+    @Override
     public List<Employee> FindAllByJobPosition(String jobPosition){
         return employeeRespository.findByJobPosition(jobPosition);
     }
 
+    @Override
     public Employee saveEmployee(Employee employee){
         return employeeRespository.save(employee);
     }
 
+    @Override
     public void deleteEmployee(Long idEmployee){
         employeeRespository.deleteById(idEmployee);
     }
 
+    @Override
+    @Transactional
     public void updateEmail(Long idEmployee, String email){
+        employeeRespository.updateEmail(idEmployee, email);
+    }
+
+    @Override
+    @Transactional
+    public void updateNumberPhone(Long idEmployee, Long phoneNumber){
+        employeeRespository.updatePhoneNumber(idEmployee, phoneNumber);
+    }
+
+    @Override
+    @Transactional
+    public void updateContractType(Long idEmployee, String contractType){
+        employeeRespository.updateContractType(idEmployee, contractType);
     }
 }
