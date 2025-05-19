7f46b39779f6d18bab00fb282f74d55374cb5849
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-05-18 19:37:22 -0500

    Creacion de clases dto

---
+++ msvc-sav-workstaff/src/main/java/com/jeferson/msvc_sav_workstaff/dto/EmployeeDto.java
@@ -0,0 +1,25 @@
+package com.jeferson.msvc_sav_workstaff.dto;
+
+import java.time.LocalDate;
+import com.jeferson.msvc_sav_workstaff.models.ContractType;
+import com.jeferson.msvc_sav_workstaff.models.JobPosition;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeDto {
+
+    private String name;
+    private String lastName;
+    private String documentType;
+    private Long documentNumber;
+    private LocalDate dateOfBirth;
+    private String email;
+    private Long phoneNumber;
+    private ContractType contractType;
+    private JobPosition jobPosition;
+    private Boolean workStatus;
+}
