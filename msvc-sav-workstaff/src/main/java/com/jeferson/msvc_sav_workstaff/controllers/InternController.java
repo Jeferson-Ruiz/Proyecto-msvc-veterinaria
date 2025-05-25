828708d3d9d18bc79a971dcb25211672c66b81bc
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-05-25 16:36:42 -0500

    Creacion clase controller

---
+++ msvc-sav-workstaff/src/main/java/com/jeferson/msvc_sav_workstaff/controllers/InternController.java
@@ -0,0 +1,36 @@
+package com.jeferson.msvc_sav_workstaff.controllers;
+
+import java.util.Optional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.jeferson.msvc_sav_workstaff.dto.InternDto;
+import com.jeferson.msvc_sav_workstaff.mapper.InternMapper;
+import com.jeferson.msvc_sav_workstaff.models.Intern;
+import com.jeferson.msvc_sav_workstaff.services.InternService;
+
+@RestController
+@RequestMapping("api/sav/employee/intern")
+public class InternController {
+
+    private final InternService intService;
+    private final InternMapper intMapper;
+
+    public InternController(InternService intService, InternMapper intMapper) {
+        this.intService = intService;
+        this.intMapper = intMapper;
+    }
+
+    public ResponseEntity<?> saveInfoIntern(InternDto internDto) {
+        Optional<Intern> optInter = intService.saveIntern(internDto);
+
+        if (optInter.isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body("Usario ya existe en el sistema");
+        }
+        Intern internResgister = optInter.get();
+        InternDto internResponse = intMapper.toDto(internResgister);
+        return ResponseEntity.status(HttpStatus.CREATED).body(internResponse);
+    }
+}
