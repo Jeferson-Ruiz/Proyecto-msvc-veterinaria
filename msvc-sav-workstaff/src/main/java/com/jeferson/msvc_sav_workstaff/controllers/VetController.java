167d6b2e750470b853af7ae77e99ada257c33aea
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-05-25 19:14:06 -0500

    Creacion de clase controller

---
+++ msvc-sav-workstaff/src/main/java/com/jeferson/msvc_sav_workstaff/controllers/VetController.java
@@ -0,0 +1,41 @@
+package com.jeferson.msvc_sav_workstaff.controllers;
+
+import java.util.Optional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.jeferson.msvc_sav_workstaff.dto.VetDto;
+import com.jeferson.msvc_sav_workstaff.mapper.VetMapper;
+import com.jeferson.msvc_sav_workstaff.models.Vet;
+import com.jeferson.msvc_sav_workstaff.services.VetService;
+
+@RestController
+@RequestMapping("api/sav/employee/vet")
+public class VetController {
+
+    private final VetService vetService;
+    private final VetMapper vetMapper;
+
+    public VetController(VetService vetService, VetMapper vetMapper) {
+        this.vetService = vetService;
+        this.vetMapper = vetMapper;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> saveInfoVet(@RequestBody VetDto vetDto) {
+        Optional<Vet> optVet = vetService.saveVet(vetDto);
+
+        if (optVet.isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body("usuario ya existe en el sistema");
+        }
+
+        Vet vetRegist = optVet.get();
+        VetDto vetResponse = vetMapper.toDto(vetRegist);
+        return ResponseEntity.status(HttpStatus.CREATED).body(vetResponse);
+    }
+
+}
