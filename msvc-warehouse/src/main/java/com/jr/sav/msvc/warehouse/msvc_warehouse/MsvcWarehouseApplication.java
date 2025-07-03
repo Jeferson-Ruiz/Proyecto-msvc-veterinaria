fc9be991b38f02163c5b037e218fcd869b4b4820
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2025-07-02 20:31:08 -0500

    Creacion de proyecto warehouse

---
+++ msvc-warehouse/src/main/java/com/jr/sav/msvc/warehouse/msvc_warehouse/MsvcWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.jr.sav.msvc.warehouse.msvc_warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcWarehouseApplication.class, args);
+	}
+
+}
---
+++ msvc-warehouse/src/test/java/com/jr/sav/msvc/warehouse/msvc_warehouse/MsvcWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jr.sav.msvc.warehouse.msvc_warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
