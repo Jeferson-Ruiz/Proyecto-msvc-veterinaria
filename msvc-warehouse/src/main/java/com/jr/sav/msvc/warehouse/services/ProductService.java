380ea0d6ffeae2d43eb6e0e4fcc0c3d0417dcdb6
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-07-09 14:38:09 -0500

    Trabajando en la clase service

--- msvc-warehouse/src/main/java/com/jr/sav/msvc/warehouse/services/ProductService.java
+++ msvc-warehouse/src/main/java/com/jr/sav/msvc/warehouse/services/ProductService.java
@@ -1,5 +1,9 @@
 package com.jr.sav.msvc.warehouse.services;
 
+import java.util.List;
+import com.jr.sav.msvc.warehouse.dto.ProductDto;
+
 public interface ProductService {
+    List<ProductDto> getAllProducts();
 
 }
--- msvc-warehouse/src/main/java/com/jr/sav/msvc/warehouse/services/ProductServiceImpl.java
+++ msvc-warehouse/src/main/java/com/jr/sav/msvc/warehouse/services/ProductServiceImpl.java
@@ -1,10 +1,14 @@
 package com.jr.sav.msvc.warehouse.services;
 
 import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
 import com.jr.sav.msvc.warehouse.dto.ProductDto;
+import com.jr.sav.msvc.warehouse.entities.Product;
 import com.jr.sav.msvc.warehouse.mapper.ProductMapper;
 import com.jr.sav.msvc.warehouse.repositories.ProductRepository;
 
+@Service
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
@@ -15,13 +19,21 @@ public class ProductServiceImpl implements ProductService {
         this.productMapper = productMapper;
     }
 
-
-    public List<ProductDto> getProducts(){
+    @Override
+    public List<ProductDto> getAllProducts(){
         return productRepository.findAll().stream()
             .map(productMapper::toDto).toList();
     }
-    
 
+    @Override
+    public Optional<ProductDto> findById(Long id){
+        Optional<Product> optProduct = productRepository.findById(id);
+        if (optProduct.isEmpty()) {
+            return Optional.empty();
+        }
+        ProductDto dto = productMapper.toDto(optProduct.get());
+        return Optional.of(dto);
+    }
 
 
 
