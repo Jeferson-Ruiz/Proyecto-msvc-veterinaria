709c1e38e8c73fda972f7b9baa5742b9a5742cae
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-07-13 20:38:07 -0500

    Metodos en repositorio

--- msvc-warehouse/src/main/java/com/jr/sav/msvc/warehouse/repositories/ProductRepository.java
+++ msvc-warehouse/src/main/java/com/jr/sav/msvc/warehouse/repositories/ProductRepository.java
@@ -1,6 +1,5 @@
 package com.jr.sav.msvc.warehouse.repositories;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,9 +8,15 @@ import com.jr.sav.msvc.warehouse.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    
     @Modifying
     @Query("update Product set quantityStock=:quantityStock where id=:id")
-    void updateQuantityStock (@Param("id") Long id, @Param("quantityStock") Long quantityStock);
+    void updateQuantityStock(@Param("id") Long id, @Param("quantityStock") Long quantityStock);
 
+    @Modifying
+    @Query("update Product set purchasePrice=:purchasePrice WHERE id=:id")
+    void updatePurchasePrice(@Param("id") Long id, @Param("purchasePrice") Double purchasePrice);
+
+    @Modifying
+    @Query("update Product set salesPrice=:salesPrice Where id=:id")
+    void updateSalesPrice(@Param("id") Long id, @Param("salesPrice") Double salesPrice);
 }
