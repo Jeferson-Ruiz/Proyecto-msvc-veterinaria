d41e87bed072c208e1493c620a1ff9f9ebea47d2
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-08-06 15:18:32 -0500

    refactorizar clase Consultation

--- sav-mvsc-medicalcontrol/src/main/java/com/jr/sav_mvsc_medicalcontrol/controllers/ConsultationController.java
+++ sav-mvsc-medicalcontrol/src/main/java/com/jr/sav_mvsc_medicalcontrol/controllers/ConsultationController.java
@@ -2,12 +2,10 @@ package com.jr.sav_mvsc_medicalcontrol.controllers;
 
 import com.jr.sav_mvsc_medicalcontrol.dto.ConsultationDto;
 import com.jr.sav_mvsc_medicalcontrol.services.ConsultationService;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("api/sav/consultation")
@@ -25,43 +23,26 @@ public class ConsultationController {
     }
 
     @GetMapping("/id/{idConsultation}")
-    public ResponseEntity<?> getConsultationBYId(@PathVariable Long idConsultation) {
-        Optional<ConsultationDto> optConsultatio = consultationService.finConsultionById(idConsultation);
-        if (optConsultatio.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body("No existe resgistro de consulta con el ID: "+ idConsultation+ " en el sistema");
-        }
-        return ResponseEntity.ok(optConsultatio.get());
+    public ResponseEntity<?> getConsultationById(@PathVariable Long idConsultation) {
+        ConsultationDto consultation = consultationService.findConsultionById(idConsultation);
+        return ResponseEntity.ok(consultation);
     }
 
     @GetMapping("/idpet/{idPet}")
     public ResponseEntity<?> getConsultationByIdPet(@PathVariable Long idPet){
-        Optional<ConsultationDto> optConsultation = consultationService.findConsultationByIdPet(idPet);
-        if(optConsultation.isEmpty()){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body("No existe consulta asociada con el paciente: "+ idPet+ " en el sistema");    
-        }
-        return ResponseEntity.ok(optConsultation.get());
+        ConsultationDto consultation = consultationService.findConsultationByIdPet(idPet);
+        return ResponseEntity.ok(consultation);
     }
 
     @PostMapping
     public ResponseEntity<?> saveInfoConsultation(@RequestBody ConsultationDto consultationDto) {
-        Optional<ConsultationDto> consultationOpt = consultationService.saveConsultation(consultationDto);
-        if (consultationOpt.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.CONFLICT)
-                    .body("Error, no se pueden registrar consultas porque el paciente "
-                            + consultationDto.getIdPet() + " no existe en el sistema");
-        }
-        return ResponseEntity.status(HttpStatus.CREATED).body(consultationOpt);
+        ConsultationDto savedConsultation = consultationService.saveConsultation(consultationDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedConsultation);
     }
 
     @DeleteMapping("id/{idConsultation}")
     public ResponseEntity<?> deleteInfoConsultation(@PathVariable Long idConsultation) {
-        try {
-            consultationService.deleteConsultation(idConsultation);
-            return ResponseEntity.noContent().build();
-        } catch (EntityNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body("Error, no existe resgistro de consulta con el ID: "+ idConsultation+ " en el sistema");        }
+        consultationService.deleteConsultation(idConsultation);
+        return ResponseEntity.noContent().build();
     }
 }
--- sav-mvsc-medicalcontrol/src/main/java/com/jr/sav_mvsc_medicalcontrol/repositories/ConsultationRepository.java
+++ sav-mvsc-medicalcontrol/src/main/java/com/jr/sav_mvsc_medicalcontrol/repositories/ConsultationRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.jr.sav_mvsc_medicalcontrol.models.Consultation;
 
 public interface ConsultationRepository extends JpaRepository<Consultation, Long> {
+
     Optional<Consultation> findByIdPet(Long idPet);
 }
