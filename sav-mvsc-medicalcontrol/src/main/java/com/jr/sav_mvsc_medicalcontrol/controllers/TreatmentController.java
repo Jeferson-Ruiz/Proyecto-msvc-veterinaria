64d3bed79b391dcb890847906a0ea1fc6086deb5
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-08-06 15:19:03 -0500

    Refactorizar controller Treatment

--- sav-mvsc-medicalcontrol/src/main/java/com/jr/sav_mvsc_medicalcontrol/controllers/TreatmentController.java
+++ sav-mvsc-medicalcontrol/src/main/java/com/jr/sav_mvsc_medicalcontrol/controllers/TreatmentController.java
@@ -1,10 +1,8 @@
 package com.jr.sav_mvsc_medicalcontrol.controllers;
 
 import java.util.List;
-import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.jr.sav_mvsc_medicalcontrol.dto.TreatmentDto;
 import com.jr.sav_mvsc_medicalcontrol.services.TreatmentService;
-import jakarta.persistence.EntityNotFoundException;
 
 @RestController
 @RequestMapping("api/sav/treatment")
@@ -32,33 +29,19 @@ public class TreatmentController {
 
     @GetMapping("idtreatment/{idTreatment}")
     public ResponseEntity<?> getTreatmentById(@PathVariable Long idTreatment){
-        Optional<TreatmentDto> optTreatment = treatmentService.findTreatmentById(idTreatment);
-        if (optTreatment.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-            .body("El tratamiento: "+idTreatment+" no existe en el sistema");
-        }
-        return ResponseEntity.ok(optTreatment.get());
+        TreatmentDto treatmentDto = treatmentService.findTreatmentById(idTreatment);
+        return ResponseEntity.ok(treatmentDto);   
     }
 
     @PostMapping
     public ResponseEntity<?> saveInfoTreatment(@RequestBody TreatmentDto treatmentdDto){
-        Optional<TreatmentDto> optTreatment = treatmentService.saveTreatment(treatmentdDto);
-        if (optTreatment.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body("Error, no se pueden registrar tratamiento porque el paciente no cuenta con cnsulta");
-        }
-        return ResponseEntity.ok(optTreatment);
+        TreatmentDto treatmentDto = treatmentService.saveTreatment(treatmentdDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(treatmentDto);
     }
 
 
-    @DeleteMapping("idtreatment/{idTreatment}")
-    public ResponseEntity<?> deleteInfoTreatment(@PathVariable Long idTreatment){
-        try {
-            treatmentService.deleteTreatment(idTreatment);
-            return ResponseEntity.noContent().build();
-        } catch (EntityNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body("Error, no se pueden eliminar tratamiento "+ idTreatment +" no existe en el sistema");
-        }
-    }
+    // @DeleteMapping("idtreatment/{idTreatment}")
+    // public ResponseEntity<?> deleteInfoTreatment(@PathVariable Long idTreatment){
+    //     treatmentService.
+    // }
 }
