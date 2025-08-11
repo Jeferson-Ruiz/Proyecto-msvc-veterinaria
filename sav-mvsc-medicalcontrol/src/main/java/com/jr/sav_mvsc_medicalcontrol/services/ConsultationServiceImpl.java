5408be29977070c59c11fa4d49aaa47bd111ec5e
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-08-11 10:33:53 -0500

    Agregando validacion por horario

--- sav-mvsc-medicalcontrol/src/main/java/com/jr/sav_mvsc_medicalcontrol/services/ConsultationServiceImpl.java
+++ sav-mvsc-medicalcontrol/src/main/java/com/jr/sav_mvsc_medicalcontrol/services/ConsultationServiceImpl.java
@@ -1,9 +1,12 @@
 package com.jr.sav_mvsc_medicalcontrol.services;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import com.jr.sav_mvsc_medicalcontrol.dto.ConsultationDto;
+import com.jr.sav_mvsc_medicalcontrol.dto.ConsultationReponseDto;
 import com.jr.sav_mvsc_medicalcontrol.mapper.ConsultationMapper;
 import com.jr.sav_mvsc_medicalcontrol.models.Consultation;
 import com.jr.sav_mvsc_medicalcontrol.models.Pet;
@@ -18,6 +21,10 @@ public class ConsultationServiceImpl implements ConsultationService {
     private final ConsultationRepository consultationRepository;
     private final ConsultationMapper consultationMapper;
 
+    // Horario laboral
+    LocalTime startTime = LocalTime.of(8, 0); // 8:00 AM
+    LocalTime endTime = LocalTime.of(17, 30); // 6:00 PM
+
     public ConsultationServiceImpl(PetRepository petRepository, ConsultationRepository consultationRepository,
             ConsultationMapper consultationMapper) {
         this.petRepository = petRepository;
@@ -26,52 +33,77 @@ public class ConsultationServiceImpl implements ConsultationService {
     }
 
     @Override
-    public List<ConsultationDto> findAllConsultations() {
+    public List<ConsultationReponseDto> findAllConsultations() {
         return consultationRepository.findAll().stream()
                 .map(consultationMapper::toDto).toList();
     }
 
     @Override
-    public ConsultationDto findConsultionById(Long idConsultation) {
+    public ConsultationReponseDto findConsultionById(Long idConsultation) {
         Consultation consultation = consultationRepository.findById(idConsultation)
                 .orElseThrow(() -> new EntityNotFoundException(
-                        "No se encontro consulta asociada al paciente " + idConsultation));
+                        "No se encontro consulta asociada al id " + idConsultation));
         return consultationMapper.toDto(consultation);
     }
 
     @Override
-    public ConsultationDto saveConsultation(ConsultationDto consultationDto) {
+    public ConsultationReponseDto saveConsultation(ConsultationDto consultationDto) {
         Pet pet = petRepository.findById(consultationDto.getIdPet())
-            .orElseThrow(() -> new RuntimeException("No se puede generar una consulta para la mascota "+ consultationDto.getIdPet()));
+                .orElseThrow(() -> new RuntimeException(
+                        "No se puede generar una consulta para la mascota " + consultationDto.getIdPet()));
+
+        if (!pet.getActive()) {
+            throw new RuntimeException("No se puede generar una consuta el paciente " + consultationDto.getIdPet()
+                    + " se encuentra desactivado");
+        }
+
+        LocalTime dateTime = consultationDto.getCitationDate().toLocalTime();
+        validarHora(dateTime);
 
         Consultation consultation = consultationMapper.toEntity(consultationDto);
+        consultation.setPet(pet);
         consultation.setRegistrationDate(LocalDateTime.now());
         Consultation savedConsultation = consultationRepository.save(consultation);
         return consultationMapper.toDto(savedConsultation);
     }
 
     @Override
-    public void deleteConsultation(Long idConsultation) {
-        Consultation consultation = consultationRepository.findById(idConsultation)
-                .orElseThrow(() -> new EntityNotFoundException("No existe consulta asociada al Id " + idConsultation));
-        consultationRepository.delete(consultation);
-    }
-
-    @Override
-    public ConsultationDto findConsultationByIdPet(Long idPet) {
-        Consultation consultatio = consultationRepository.findByIdPet(idPet)
+    public ConsultationReponseDto findConsultationByIdPet(Long idPet) {
+        Consultation consultatio = consultationRepository.findFirstByPetIdPetOrderByCitationDateDesc(idPet)
                 .orElseThrow(() -> new EntityNotFoundException(
-                        "No existe consulta asociada al paciente con el Id: " + idPet +" en el sistema"));
-
+                        "No existe consulta asociada al paciente con el Id: " + idPet + " en el sistema"));
         return consultationMapper.toDto(consultatio);
     }
 
     @Override
-    public List<ConsultationDto> findAllConsultationById(Long idPet){
+    public List<ConsultationReponseDto> findAllConsultationById(Long idPet) {
         List<Consultation> consultations = consultationRepository.findAllByIdPet(idPet);
         if (consultations.isEmpty()) {
-            throw new EntityNotFoundException("No se encontraron consultas asociadas al paciente "+ idPet);
+            throw new EntityNotFoundException("No se encontraron consultas asociadas al paciente " + idPet);
         }
         return consultations.stream().map(consultationMapper::toDto).toList();
+    }
+
+    @Override
+    @Transactional
+    public void updateConsultationDate(Long idConsultation, LocalDateTime newDate) {
+        Consultation consultation = consultationRepository.findById(idConsultation)
+            .orElseThrow(
+                () -> new IllegalArgumentException("No se encontro consulta asocida al id" + idConsultation));
+
+        if (!newDate.isAfter(consultation.getCitationDate())) {
+            throw new IllegalArgumentException("La nueva fecha debe ser posterior a la actual");
+        }
+
+        LocalTime dateTime = newDate.toLocalTime();
+        validarHora(dateTime);
+
+        consultationRepository.updateCitationDate(idConsultation, newDate);
+    }
+
+    private void validarHora(LocalTime date) {
+        if (date.isBefore(startTime) || date.isAfter(endTime)) {
+            throw new RuntimeException("Las cita deben ser programardas entre las " + startTime + " y las " + endTime);
+        }
     }
 }
