04d3f2d6619acb63255aae5102bc327f008e8124
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-08-11 10:35:32 -0500

    Creacion de Dto para formato fecha y hora

---
+++ sav-mvsc-medicalcontrol/src/main/java/com/jr/sav_mvsc_medicalcontrol/dto/ConsultationDateUpdate.java
@@ -0,0 +1,17 @@
+package com.jr.sav_mvsc_medicalcontrol.dto;
+
+import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ConsultationDateUpdate {
+
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    private LocalDateTime newDate;
+
+}
