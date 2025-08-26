6bac4b25b5a47988b84066417795e32eadb4cc14
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-08-26 16:47:31 -0500

    Creacion de enum WorkArea

---
+++ msvc-sav-workstaff/src/main/java/com/jeferson/msvc_sav_workstaff/models/WorkArea.java
@@ -0,0 +1,14 @@
+package com.jeferson.msvc_sav_workstaff.models;
+
+public enum WorkArea {
+    GENERAL_MEDICINE,
+    SURGERY,
+    THERAPY,
+    VETERINARY_ASSISTANCE,
+    ADMINISTRATION,
+    ACCOUNTING,
+    INVENTORY,
+    RECEPTION,
+    SALES,
+    CLEANING
+}
