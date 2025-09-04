395fab2499989a4d68253d3d88b5bbbd676f36ab
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-09-04 11:56:27 -0500

    Agreganado area de trabajos

--- msvc-sav-workstaff/src/main/java/com/jeferson/msvc_sav_workstaff/models/WorkArea.java
+++ msvc-sav-workstaff/src/main/java/com/jeferson/msvc_sav_workstaff/models/WorkArea.java
@@ -1,14 +1,8 @@
 package com.jeferson.msvc_sav_workstaff.models;
 
 public enum WorkArea {
-    GENERAL_MEDICINE,
-    SURGERY,
-    THERAPY,
-    VETERINARY_ASSISTANCE,
-    ADMINISTRATION,
-    ACCOUNTING,
-    INVENTORY,
-    RECEPTION,
-    SALES,
-    CLEANING
+    ADMINISTRATIVE,
+    AUXILIARY,
+    INTERN,
+    VET
 }
