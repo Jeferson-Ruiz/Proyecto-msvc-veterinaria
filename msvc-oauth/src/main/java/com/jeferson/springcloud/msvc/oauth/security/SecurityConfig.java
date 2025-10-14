d394092006057b0151a53adc51d02e57eef3ad46
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2025-10-14 07:43:26 -0500

    Refactorizzando el SecurityConfig para trabajar con roles personalizados

--- msvc-oauth/src/main/java/com/jeferson/springcloud/msvc/oauth/security/SecurityConfig.java
+++ msvc-oauth/src/main/java/com/jeferson/springcloud/msvc/oauth/security/SecurityConfig.java
@@ -4,24 +4,31 @@ import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
@@ -29,13 +36,18 @@ import org.springframework.security.oauth2.server.authorization.config.annotatio
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@Bean
 	@Order(1)
@@ -76,11 +88,13 @@ public class SecurityConfig {
 			)
 			// Form login handles the redirect to the login page from the
 			// authorization server filter chain
+			.csrf(csrf -> csrf.disable())
 			.formLogin(Customizer.withDefaults());
 
 		return http.build();
 	}
 
+	/*
 	@Bean
 	UserDetailsService userDetailsService() {
 		UserDetails userDetails = User.builder()
@@ -96,16 +110,18 @@ public class SecurityConfig {
 				.build();
 		return new InMemoryUserDetailsManager(userDetails,admin);
 	}
+	*/
 
 	@Bean
 	RegisteredClientRepository registeredClientRepository() {
 		RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
 				.clientId("gateway-app")
-				.clientSecret("{noop}12345")
+				.clientSecret(passwordEncoder.encode("12345"))
+				//.clientSecret("{noop}12345")
 				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
 				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
 				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-				.redirectUri("http://127.0.0.1:8090/login/oauth2/code/gateway-app")
+				.redirectUri("http://127.0.0.1:8090/login/oauth2/code/client-app")
 				.redirectUri("http://127.0.0.1:8090/authorized")
 				.postLogoutRedirectUri("http://127.0.0.1:8090/logout")
 				.scope(OidcScopes.OPENID)
@@ -150,6 +166,21 @@ public class SecurityConfig {
 	@Bean
 	AuthorizationServerSettings authorizationServerSettings() {
 		return AuthorizationServerSettings.builder().build();
+	}
+
+	//metodo para agregar roles e informorcaion al jwt
+	@Bean
+	OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer(){
+		return context -> {
+			if (context.getTokenType().getValue() == OAuth2TokenType.ACCESS_TOKEN.getValue()) {
+				Authentication principal = context.getPrincipal();
+				context.getClaims()
+						.claim("roles", principal.getAuthorities()
+						.stream()
+						.map(GrantedAuthority::getAuthority)
+						.collect(Collectors.toList()));
+			}
+		};
 	}
 
 }
