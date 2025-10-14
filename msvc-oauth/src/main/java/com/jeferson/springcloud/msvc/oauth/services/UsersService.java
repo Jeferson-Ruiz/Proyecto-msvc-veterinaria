60b8333b3a219cc06d7bcc14e72d89dc2e0b8e91
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2025-10-14 07:46:10 -0500

    Implementando UserDetailsService para generar el login con los roles personalizados

---
+++ msvc-oauth/src/main/java/com/jeferson/springcloud/msvc/oauth/services/UsersService.java
@@ -0,0 +1,67 @@
+package com.jeferson.springcloud.msvc.oauth.services;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
+import com.jeferson.springcloud.msvc.oauth.models.User;
+import com.jeferson.springcloud.msvc.oauth.models.UserStatus;
+import jakarta.ws.rs.NotFoundException;
+
+@Service
+public class UsersService implements UserDetailsService {
+
+    private final WebClient.Builder client;
+
+    public UsersService(Builder client) {
+        this.client = client;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Map<String, String> params = new HashMap<>();
+        params.put("username", username);
+
+        try {
+            User user = client.build().get().uri("username/{username}", params)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(User.class)
+                .block();
+
+            List<GrantedAuthority> roles = user.getRoles()
+                                                .stream()
+                                                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                                                .collect(Collectors.toList());
+            
+            boolean enabled = user.getStatus() == UserStatus.ACTIVE;
+
+            System.out.println(roles); 
+            System.out.println(username);
+            System.out.println(user.getPassword());
+            
+            return new org.springframework.security.core.userdetails.User(user.getUsername(),
+                                                                    user.getPassword(),
+                                                                    enabled,
+                                                                    true,
+                                                                    true,
+                                                                    true,
+                                                                    roles);
+
+        } catch (WebClientRequestException e) {
+            throw new NotFoundException("Error de login, username "+ username +" no existe");
+        }
+    }
+
+}
