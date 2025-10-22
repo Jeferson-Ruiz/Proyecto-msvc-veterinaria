e867ed68ea364258a659b132c51c01bcaa28dda7
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2025-10-22 10:41:00 -0500

    refactorizar para captar la  infromacion de users a oauth

--- msvc-oauth/src/main/java/com/jeferson/springcloud/msvc/oauth/services/UsersService.java
+++ msvc-oauth/src/main/java/com/jeferson/springcloud/msvc/oauth/services/UsersService.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientRequestException;
@@ -21,10 +22,13 @@ import jakarta.ws.rs.NotFoundException;
 @Service
 public class UsersService implements UserDetailsService {
 
+    private final PasswordEncoder passwordEncoder;
+
     private final WebClient.Builder client;
 
-    public UsersService(Builder client) {
+    public UsersService(Builder client, PasswordEncoder passwordEncoder) {
         this.client = client;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
