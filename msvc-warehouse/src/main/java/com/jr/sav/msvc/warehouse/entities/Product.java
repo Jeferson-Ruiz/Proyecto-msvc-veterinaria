b88efa1189d4c658515c277b0f08df1a766752d3
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2025-11-14 11:56:32 -0500

    Refactorizando la entidad implmentando relacion M a 1 con Categoria

--- msvc-warehouse/src/main/java/com/jr/sav/msvc/warehouse/entities/Product.java
+++ msvc-warehouse/src/main/java/com/jr/sav/msvc/warehouse/entities/Product.java
@@ -1,14 +1,16 @@
 package com.jr.sav.msvc.warehouse.entities;
 
-import java.time.LocalDate;
-
+import java.time.LocalDateTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,14 +27,17 @@ public class Product{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "pro_commercial_name", nullable = false, length = 30)
-    private String commercialName;
+    @Column(name = "pro_code", nullable = false, unique = true, length = 20)
+    private String productCode;
 
-    @Column(name = "pro_mark", nullable = false, length = 50)
-    private String mark;
+    @Column(name = "pro_commercial_name", nullable = false, length = 30)
+    private String productName;
     
-    @Column(name = "pro_quantity_stock", nullable = false)
-    private Integer quantityStock;
+    @Column(name = "pro_subgroup", nullable = false, length = 30)
+    private String subgroup;  
+
+    @Column(name = "pro_mark", nullable = false)
+    private String productBrand;
 
     @Column(name = "pro_presentation")
     private String presentation;
@@ -40,13 +45,20 @@ public class Product{
     @Column(name = "pro_purchase_price", nullable = false)
     private Double purchasePrice;
 
-    @Column(name = "pro_purchase_price", nullable = false)
+    @Column(name = "pro_sales_price", nullable = false)
     private Double salesPrice;
 
-    @Column(name = "pro_category", nullable = false)
+    @Column(name = "pro_quantity_stock", nullable = false)
+    private Integer quantityStock;
+
+    @Column(name = "pro_activo", nullable = false)
     @Enumerated(EnumType.STRING)
-    private Category category;
+    private ProductStatus status;
 
     @Column(name = "med_purchase_price", nullable = false)
-    private LocalDate registrationDate;
+    private LocalDateTime registrationDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cat_id", nullable = false)
+    private Category category;
 }
