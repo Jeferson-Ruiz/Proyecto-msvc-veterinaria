4db718b428d72a853831b02cb72a52f08ce293a2
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2025-11-17 08:47:24 -0500

    Creacion de micro servicio facturacion

---
+++ msvc-invoice/src/main/java/com/msvc/invoice/MsvcInvoiceApplication.java
@@ -0,0 +1,13 @@
+package com.msvc.invoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcInvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcInvoiceApplication.class, args);
+	}
+
+}
