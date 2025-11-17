938e1037c8e5e8d9328317682b1ce6c9df141351
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2025-11-17 12:45:25 -0500

    Agregando validacion para que se vean los mensajes personalizados

--- msvc-warehouse/src/main/java/com/jr/sav/msvc/warehouse/exeptions/GlobalExeptions.java
+++ msvc-warehouse/src/main/java/com/jr/sav/msvc/warehouse/exeptions/GlobalExeptions.java
@@ -1,6 +1,10 @@
 package com.jr.sav.msvc.warehouse.exeptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import jakarta.persistence.EntityNotFoundException;
@@ -22,5 +26,16 @@ public class GlobalExeptions {
     public ResponseEntity<String> handleRuntimeExeption(RuntimeException ex){
         return ResponseEntity.badRequest().body(ex.getMessage());
     }
+
+    //Mensajes para las validaciones @Valid
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+
 
 }
