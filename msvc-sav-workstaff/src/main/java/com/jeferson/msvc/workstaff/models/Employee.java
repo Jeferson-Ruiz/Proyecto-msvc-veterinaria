0852d8eabe590d14fa502af7cde85f03bd17a42f
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2025-12-04 10:30:00 -0500

    Refactorizando entidad para que implemente realcion M a M con Roles

--- msvc-sav-workstaff/src/main/java/com/jeferson/msvc/workstaff/models/Employee.java
+++ msvc-sav-workstaff/src/main/java/com/jeferson/msvc/workstaff/models/Employee.java
@@ -1,19 +1,21 @@
 package com.jeferson.msvc.workstaff.models;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
-import jakarta.persistence.DiscriminatorColumn;
-import jakarta.persistence.DiscriminatorType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -26,10 +28,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn(name = "emp_job_position", discriminatorType = DiscriminatorType.STRING)
-@Table(name = "employees")
-public abstract class Employee {
+@Table(name = "employee")
+public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,6 +39,7 @@ public abstract class Employee {
     @Column(name = "emp_code", nullable = false, unique = true)
     private String employeeCode;
 
+    //Información de Empleado
     @Column(name = "emp_name", length = 20, nullable = false)
     private String name;
 
@@ -52,27 +53,37 @@ public abstract class Employee {
     @Column(name = "emp_docu_number", nullable = false, unique = true)
     private String documentNumber;
 
-    @Column(name = "emp_date_of_birth", nullable = false)
-    private LocalDate dateOfBirth;
-
     @Column(name = "emp_email", nullable = false, length = 50)
     private String email;
 
-    @Column(name = "emp_phone_number", nullable = false, length = 10)
+    @Column(name = "emp_phone_number", nullable = false, length = 15)
     private String phoneNumber;
+
+    @Column(name = "emp_date_birth", nullable = false)
+    private LocalDate dateOfBirth;
+
+    //Estado laboral
+    @Column(name = "emp_wotk_area")
+    @Enumerated(EnumType.STRING)
+    private WorkArea workArea;
 
     @Column(name = "emp_contract_type", nullable = false)
     @Enumerated(EnumType.STRING)
     private ContractType contractType;
-    
-    @Column(name = "emp_registration_date")
-    private LocalDate registrationDate;
 
     @Column(name = "emp_work_status", nullable = false)
     @Enumerated(EnumType.STRING)
     private EmployeeStatus status;
 
-    public abstract WorkArea getArea();
+    @Column(name = "emp_registration_date")
+    private LocalDate registrationDate;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "employee_role",
+        joinColumns = @JoinColumn(name = "emp_id"),
+        inverseJoinColumns = @JoinColumn(name = "rol_id"))
+    private Set<Role> roles = new HashSet<>();
 
     @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL)
     private List<ActionInformation> actionInformations;
