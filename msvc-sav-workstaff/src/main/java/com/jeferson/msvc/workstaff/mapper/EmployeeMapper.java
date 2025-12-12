9a2eddc5a4d78cdea66051c1a4fb1b64b6dd3e77
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2025-12-12 12:17:53 -0500

    Rrfactorizando mapper para retornar roles

--- msvc-sav-workstaff/src/main/java/com/jeferson/msvc/workstaff/mapper/EmployeeMapper.java
+++ msvc-sav-workstaff/src/main/java/com/jeferson/msvc/workstaff/mapper/EmployeeMapper.java
@@ -2,11 +2,16 @@ package com.jeferson.msvc.workstaff.mapper;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import com.jeferson.msvc.workstaff.dto.EmployeeRequestDto;
 import com.jeferson.msvc.workstaff.dto.EmployeeResponseDto;
 import com.jeferson.msvc.workstaff.models.Employee;
+import com.jeferson.msvc.workstaff.models.Role;
 
 @Mapper(componentModel = "spring")
 public interface EmployeeMapper {
@@ -14,7 +19,16 @@ public interface EmployeeMapper {
     Employee toEntity(EmployeeRequestDto employee);
 
     @Mapping(target = "fullName", expression = "java(employee.getName() + \" \" + employee.getLastName())")
+    @Mapping(target = "roles", source = "employee.roles", qualifiedByName = "mapRolesToCodes")
     EmployeeResponseDto toDto(Employee employee);
+
+    @Named("mapRolesToCodes")
+    default List<String> mapRolesToCodes(Set<Role> roles) {
+        if (roles == null) return null;
+        return roles.stream()
+            .map(Role::getRoleCode)
+            .collect(Collectors.toList());
+    }
 
     default Byte calculateAge(LocalDate dateOfBirth) {
         if (dateOfBirth == null) {
